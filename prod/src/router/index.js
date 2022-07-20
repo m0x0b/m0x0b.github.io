babf3b169d5d9568e0b99a1e1b9de7324e137ae4
m <matthewh@allelo.io>
2022-07-20 03:40:18 -0400

    fixes

--- prod/src/router/index.js
+++ prod/src/router/index.js
@@ -2,6 +2,14 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Pascals0 from "../views/wall/pascals0.vue"
 import Pascals1 from "../views/wall/pascals1.vue"
+import Detritus0 from "../views/wall/detritus0.vue"
+import Detritus1 from "../views/wall/detritus1.vue"
+import Afterimage1 from "../views/wall/afterimage1.vue"
+import Afterimage0 from "../views/wall/afterimage0.vue"
+import Polyethylene1 from "../views/wall/polyethylene1.vue"
+import Polyethylene0 from "../views/wall/polyethylene0.vue"
+import Pyrolysis0 from "../views/wall/pyrolysis0.vue"
+import Pyrolysis1 from "../views/wall/pyrolysis1.vue"
 import PageNotFound from "../views/PageNotFound.vue";
 
 const routes = [
@@ -30,6 +38,78 @@ const routes = [
         meta: {
             friendlyName: "pascals1",
             title: "pascals1",
+        },
+    },
+    {
+        path: "/wall/detritus0",
+        name: "detritus0",
+        component: Detritus0,
+        meta: {
+            friendlyName: "detritus0",
+            title: "detritus0",
+        },
+    },
+    {
+        path: "/wall/detritus1",
+        name: "detritus1",
+        component: Detritus1,
+        meta: {
+            friendlyName: "Detritus1",
+            title: "Detritus0",
+        },
+    },
+    {
+        path: "/wall/afterimage0",
+        name: "afterimage0",
+        component: Afterimage0,
+        meta: {
+            friendlyName: "afterimage0",
+            title: "afterimage0",
+        },
+    },
+    {
+        path: "/wall/afterimage1",
+        name: "afterimage1",
+        component: Afterimage1,
+        meta: {
+            friendlyName: "afterimage1",
+            title: "afterimage1",
+        },
+    },
+    {
+        path: "/wall/polyethylene0",
+        name: "polyethylene0",
+        component: Polyethylene0,
+        meta: {
+            friendlyName: "polyethylene0",
+            title: "polyethylene0",
+        },
+    },
+    {
+        path: "/wall/polyethylene1",
+        name: "polyethylene1",
+        component: Polyethylene1,
+        meta: {
+            friendlyName: "polyethylene1",
+            title: "polyethylene1",
+        },
+    },
+    {
+        path: "/wall/pyrolysis0",
+        name: "pyrolysis0",
+        component: Pyrolysis0,
+        meta: {
+            friendlyName: "pyrolysis0",
+            title: "pyrolysis0",
+        },
+    },
+    {
+        path: "/wall/pyrolysis1",
+        name: "pyrolysis1",
+        component: Pyrolysis1,
+        meta: {
+            friendlyName: "pyrolysis1",
+            title: "pyrolysis1",
         },
     },
     { path: '/:pathMatch(.*)*', name: "404", component: PageNotFound },
---
+++ prod/src/views/wall/detritus1.vue
@@ -0,0 +1,42 @@
+<template>
+
+    <div class="absolute w-screen h-screen top-0 left-0 bg-[#ffffff] bg-opacity-80 text-black overflow-y-scroll">
+        <div class="flex w-full flex-col lg:flex-row">
+            <img class="w-full lg:w-[100vh] lg:h-[100vh]" src="/pieces/2-1.jpg" />
+            <div class="flex flex-col text-left flex-grow justify-center items-center uppercase relative">
+                <router-link :to="'detritus0'">
+                    <button
+                        class="absolute top-0 left-0 font-[GT] border-b border-[rgba(0,0,0,0.2)] h-16 w-full px-8 z-100 uppercase text-lg md:text-xl">View
+                        Prev in Pairing</button>
+                </router-link>
+
+                <router-link :to="'/'">
+                    <button
+                        class="absolute pb-48 bottom-0 lg:pb-0 pt-4 left-0 font-[GT] border-t border-[rgba(0,0,0,0.2)] h-16 w-full px-8 z-100 uppercase text-lg md:text-xl">View
+                        All Works</button>
+                </router-link>
+                <div class="py-24 pb-64 lg:py-0 lg:pb-0 px-4">
+                    <h1 class="text-4xl">Pascals 1</h1>
+                    <h3 class="font-[GT] mt-4">Unix Timestamp: 1652951432000</h3>
+                    <h3 class="font-[GT]">Dimensions: 36" x 36"</h3>
+                    <h3 class="font-[GT]">Medium: Print on Silk Canvas</h3>
+                    <h3 class="font-[GT]">Status: No Owner</h3>
+                    <button class="mt-4 font-[GT] uppercase  border border-black px-8 py-2 mb-2">Inquire</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+
+<script>
+import { RouterLink } from "vue-router";
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
